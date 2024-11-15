6be9f31860e2b7c4e5399eb154d27e4664e8f4d5
Nima.joudi <154692516+nimajoudi@users.noreply.github.com>
2024-11-15 01:27:11 -0500

    Update Practice2.py

--- Practice2.py
+++ Practice2.py
@@ -1,46 +1,46 @@
+
+
 from gooey import Gooey, GooeyParser
 
 @Gooey(program_name = 'Calculator')
 
 def calculator() :
-    name = input('Please enter your name : ')
-    family = input('Please enter your family : ')
     parser = GooeyParser(description = 'GUI Calculator Program')
     parser.add_argument('Name', help = 'please enter your Name : ')
     parser.add_argument('Family', help = 'please enter your Family : ')
     parser.add_argument('Number1', help = 'please enter the number : ')
     parser.add_argument('Number2', help = 'please enter the number : ')
-    parser.add_argument('Operation', help = 'please enter the operation : ')
+    parser.add_argument('Operation', choices = ['add', 'sub', 'div', 'mul'], help = 'please select your operation : ')
+    
     args = parser.parse_args()
-
-    if str(args.Name) != name and str(args.Family) != family :
-        print('Error!!! : Name or Family is incorrect')
-
-    elif int(args.Number1) and int(args.Number2) not in range (10, 99) :
-        print('Error!!! : Invalid numbers')
-
-    elif args.operation not in ('+', '-', '*', '/', '//', '**') :
-        print('Error!!! : Invalid operation')
     
-    elif args.operation == '+' :
-        print(args.Number1, '+', args.Number2, '=', int(args.Number1) + int(args.Number2))
+    var_name = 'Nima'
+    var_family = 'Joudi'
     
-    elif args.operation == '-' : 
-        print(args.Number1, '-', args.Number2, '=', int(args.Number1) - int(args.Number2))
-
-    elif args.operation == '*' : 
-        print(args.Number1, '*', args.Number2, '=', int(args.Number1) * int(args.Number2))
-
-    elif args.operation == '**' : 
-        print(args.Number1, '**', args.Number2, '=', int(args.Number1) ** int(args.Number2))
-
-    elif args.operation == '/' : 
-        print(args.Number1, '/', args.Number2, '=', int(args.Number1) / int(args.Number2))
-
-    elif args.operation == '//' : 
-        print(args.Number1, '//', args.Number2, '=', int(args.Number1) // int(args.Number2))
+    if  var_name == args.Name and var_family == args.Family and 10 <= int(args.Number1) <= 99 and 10 <= int(args.Number2) <= 99 :
+        if args.Operation == 'add' :
+            var_result_add = int(args.Number1) + int(args.Number2)
+            print('result is {}'.format(var_result_add)) 
+        
+        elif args.Operation == 'sub' :
+            var_result_add = int(args.Number1) - int(args.Number2)
+            print('result is {}'.format(var_result_add))  
+        
+        elif args.Operation == 'div' :
+            var_result_add = int(args.Number1) * int(args.Number2)
+            print('result is {}'.format(var_result_add)) 
+        
+        elif args.Operation == 'mul' :
+            var_result_add = int(args.Number1) + int(args.Number2)
+            print('result is {}'.format(var_result_add))
+        
+        else : 
+            print('Invalid Input!!!!')                                                                 
 
 calculator()
+
+
+
 
 
     
