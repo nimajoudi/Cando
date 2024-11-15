e51000228d5807b0a3310276ae7bf3756c33dd80
Nima.joudi <154692516+nimajoudi@users.noreply.github.com>
2024-11-15 02:27:53 -0500

    Update Practice4.py

--- Practice4.py
+++ Practice4.py
@@ -1,32 +1,21 @@
-from os import system
 from random import randint
 
 def guess_number() :
+    
+    pc_input = randint(1, 10)
+    
     while True :
+        user_guess = int(input('Please enter your guess number : '))
         system('clear')
-
-        while True :
-
-            #region pc input
-            
-            pc_input = randint(1, 100)
-            
-            #endregion
-
-            #region guess number   
-            while True :    
-                user_guess = int(input('Please enter your guess number : '))
-                system('clear')
                 
-                if user_guess < pc_input : 
-                    print('Wrong guess', user_guess , '< Adad e morede nazar')
+        if user_guess < pc_input : 
+            print('Wrong guess', user_guess , '< Adad e morede nazar')
+                                        
+        elif user_guess > pc_input :
+            print('Wrong guess', user_guess , '> Adad e morede nazar')
                             
-                elif user_guess > pc_input :
-                    print('Wrong guess', user_guess , '> Adad e morede nazar')
-                            
-                else:
-                    print('Wll done','Your guess : ', user_guess, 'PC number : ', pc_input, sep='\n')
-                    break
-                            
-            #endregion
+        else:
+            print('Wll done','Your guess : ', user_guess, 'PC number : ', pc_input, sep='\n')
+            break
+
 guess_number()            
